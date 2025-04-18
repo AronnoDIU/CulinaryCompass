343266ffe3dceda9d5c50df5f5fc9881fae1db85
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2025-04-18 11:26:45 +0600

    Initialize project: CulinaryCompass

---
+++ src/main/java/com/aronno/culinarycompass/CulinaryCompassApplication.java
@@ -0,0 +1,13 @@
+package com.aronno.culinarycompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinaryCompassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CulinaryCompassApplication.class, args);
+    }
+
+}
